d6e85ac7030d21823b901104a83deba6b19d5fe3
Emil Johansson <emijohansson@deloitte.com>
2018-01-20 15:45:22 -0700

    update OS. add fileSystem

--- src/apps/OS.js
+++ src/apps/OS.js
@@ -18,9 +18,36 @@ const template = `
 
 const OS = {
   template,
-  data: {
-    isActive: true
+  data: {}
+}
+
+export const fileSystem = {
+  getDir (path) {
+    if (path === '/') {
+      return fileSystem[path]
+    }
+    const paths = path.split('/').slice(1)
+    let currentDir = this['/']
+    const length = paths.length
+    let index = -1
+    while (++index < length) {
+      currentDir = currentDir[paths[index]]
+      if (currentDir == null) {
+        return
+      }
+    }
+    return currentDir
+  },
+  '/': {
+    'Applications': {},
+    'Users': {
+      'admin': []
+    }
   }
+}
+
+export const installApp = (definition, App) => {
+  fileSystem['/']['Applications'][definition] = App
 }
 
 export default OS
