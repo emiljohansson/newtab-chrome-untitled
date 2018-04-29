3a248a6382998552027ec8b57ac5fe415bb9c6e6
Emil Johansson <emijohansson@deloitte.com>
2018-04-29 13:12:24 +0200

    update main. remove test app ref

--- src/main.js
+++ src/main.js
@@ -6,28 +6,6 @@ import apps from 'core/apps'
 
 jss.setup(preset())
 
-// const composeElements = (App, elements) => {
-//   elements = toArray(elements)
-//   elements.forEach(initEl => {
-//     compose(App, initEl)
-//   })
-// }
-//
-// const appList = [
-//   require('./apps/Weather').default,
-// ]
-//
-// forEach(appList, App => {
-//   init(App)
-// })
-//
-// forEach(appList, App => {
-//   initStyle(App)
-//   composeElements(App, document.querySelectorAll(`[is=${App.reference}]`))
-// })
-
-// V2 test
-
 install('OS', require('apps/OS').default)
 install('Desktop', require('apps/Desktop').default)
 install('Window', require('apps/Window').default)
@@ -44,19 +22,5 @@ install('StatusBar', require('apps/StatusBar').default)
 install('Puzzle', require('apps/Puzzle/Puzzle').default)
 install('PuzzleItem', require('apps/Puzzle/PuzzleItem').default)
 install('Terminal', require('apps/Terminal/Terminal').default)
-install('TestApp', require('apps/TestApp').default)
-install('TestComponent', require('apps/TestComponent').default)
 
 Instance(apps('OS'), document.querySelector(`[is=OS]`))
-
-// forEach([
-//   'Time',
-//   'Weather',
-//   'Todos',
-//   'Playground',
-//   'StatusBar'
-// ], id => {
-//   forEach(document.querySelectorAll(`[is=${id}]`), el => {
-//     Instance(apps(id), el)
-//   })
-// })
